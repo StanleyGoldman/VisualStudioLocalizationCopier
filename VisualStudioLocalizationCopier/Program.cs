0704c5f571f1e3b6f7b7467a3de6bd8417abf2e3
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-05-08 08:53:04 -0400

    Actual implementation

--- VisualStudioLocalizationCopier/Program.cs
+++ VisualStudioLocalizationCopier/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace VisualStudioLocalizationCopier
 {
@@ -6,7 +8,34 @@ namespace VisualStudioLocalizationCopier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var projectPath = "C:\\Users\\Spade\\Projects\\GitHub\\VisualStudio";
+
+            var directoryInfo = new DirectoryInfo(@"\\simpleloc\drops\Drops\GitHub_VSextension_2435");
+            var latestJob = directoryInfo.GetDirectories().OrderByDescending(info => info.CreationTimeUtc).First();
+
+            var jobResultsDirectory = new DirectoryInfo(Path.Combine(latestJob.FullName, "BinDrops\\Windows\\bin"));
+            foreach (var jobLanguageResult in jobResultsDirectory.GetDirectories())
+            {
+                if(jobLanguageResult.Name == "qps-ploc") continue;
+
+                var source = Path.Combine(jobLanguageResult.FullName, "src\\GitHub.Resources\\Resources.resx");
+                var dest = Path.Combine(projectPath, "src\\GitHub.Resources", $"Resources.{jobLanguageResult.Name}.resx");
+
+                Console.WriteLine($"Copy '{source}' to '{dest}'");
+                File.Copy(source, dest, true);
+
+                source = Path.Combine(jobLanguageResult.FullName, $"src\\GitHub.VisualStudio\\xlf\\GitHub.VisualStudio.vsct.{jobLanguageResult.Name}.xlf");
+                dest = Path.Combine(projectPath, "src\\GitHub.VisualStudio\\xlf", $"GitHub.VisualStudio.vsct.{jobLanguageResult.Name}.xlf");
+
+                Console.WriteLine($"Copy '{source}' to '{dest}'");
+                File.Copy(source, dest, true);
+
+                source = Path.Combine(jobLanguageResult.FullName, $"src\\GitHub.VisualStudio\\xlf\\VSPackage.{jobLanguageResult.Name}.xlf");
+                dest = Path.Combine(projectPath, "src\\GitHub.VisualStudio\\xlf", $"VSPackage.{jobLanguageResult.Name}.xlf");
+
+                Console.WriteLine($"Copy '{source}' to '{dest}'");
+                File.Copy(source, dest, true);
+            }
         }
     }
 }
